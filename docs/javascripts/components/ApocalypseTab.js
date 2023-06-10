116a5393ebe9ca24444699a7c8621ec287796545
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2023-06-10 20:31:08 +0800

    added linebreak

--- docs/javascripts/components/ApocalypseTab.js
+++ docs/javascripts/components/ApocalypseTab.js
@@ -16,11 +16,11 @@ Vue.component("ApocalypseTab", {
     },
     template: `
     <div class="tab">
-        Your excess greed has unleashed the wrath of god in the form of apocalypses.<br>
+        Your excess greed has unleashed the wrath of god, in the form of apocalypses.<br>
         Performing an apocalypse will reset your game.<br>
         In addition, each apocalypse applies permanent nerf to your current game, 
         but also unlocks new features.<br><br>
-        You are currently in Apocalypse {{apocalypseLevel}}.<br>
+        You are currently in Apocalypse {{apocalypseLevel}}.<br><br>
         <ApocalypseDisplay v-for="apocalypse in apocalypsesList"
                            :apocalypse="apocalypse"
                            :key="apocalypse.level"/>
